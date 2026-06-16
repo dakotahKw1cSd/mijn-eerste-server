908b668e8ffc441cd3dbf0fa66832c84a5637aa4
dakotah <dakotah.vloet@edu-kw1c.nl>
2026-06-16 12:23:44 +0200

    manifest.json toegevoegd

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const PORT    = 3000;
 // Stap 2: Een route maken
 // Wanneer iemand naar "/" gaat, stuur je "Hello World!" terug
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World! 👋</h1><p>Mijn eerste server werkt!</p>');
+    res.send('<h1>Hello World! </h1><p>Mijn eerste server werkt!</p>');
 });
 
 // Stap 3: Server starten
 app.listen(PORT, () => {
-    console.log(`✅ Server draait op http://localhost:${PORT}`);
+    console.log(`Server draait op http://localhost:${PORT}`);
 });
